84ef2b152e41159975c7f779244b9736b1aa9da9
Julian Nadeau <julian.nadeau@shopify.com>
2015-01-26 13:15:24 -0500

    Fix Disqus comment count

--- app/assets/javascripts/disqus.js
+++ app/assets/javascripts/disqus.js
@@ -1,10 +1,33 @@
-/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-var disqus_shortname = 'jnadeau'; // required: replace example with your forum shortname
+$(document).on('page:change', function () {
+  var disqusPublicKey = "s8oUrLbzLsAL371XjvtXwarHriNqscuI3kAVzzc4cKXVTnixncg5g0IAMMrdfKEx"; // Replace with your own public key
+  var disqusShortname = "jnadeau"; // Replace with your own shortname
 
-/* * * DON'T EDIT BELOW THIS LINE * * */
-(function () {
-var s = document.createElement('script'); s.async = true;
-s.type = 'text/javascript';
-s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
-(document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
-}());
+  var urlArray = [];
+
+  $('.comment-link-marker').each(function () {
+    var url = $(this).attr('data-disqus-url');
+    urlArray.push('link:' + url);
+  });
+
+  $.ajax({
+    type: 'GET',
+    url: "https://disqus.com/api/3.0/threads/set.jsonp",
+    data: { api_key: disqusPublicKey, forum : disqusShortname, thread : urlArray },
+    cache: false,
+    dataType: 'jsonp',
+    success: function (result) {
+
+      for (var i in result.response) {
+
+        var countText = " comments";
+        var count = result.response[i].posts;
+
+        if (count == 1)
+          countText = " comment";
+
+        $('a[data-disqus-url="' + result.response[i].link + '"]').html(count + countText);
+
+      }
+    }
+  });
+});
