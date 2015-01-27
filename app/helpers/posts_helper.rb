5a55b2aa698b643c19bb1d3239f0eeb02fa932a6
Julian Nadeau <julian.nadeau@shopify.com>
2015-01-26 21:35:13 -0500

    Linked Tags

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,3 +1,9 @@
 module PostsHelper
   include ActsAsTaggableOn::TagsHelper
+
+  def linked_tag_list(post)
+    post.tag_list.collect do |tag|
+      link_to tag, posts_path(tagged: tag)
+    end.join.html_safe
+  end
 end
