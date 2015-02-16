0751ac4efff1a5590d2cb57b9b12494fd3292f6b
Julian Nadeau <julian@jnadeau.ca>
2015-02-16 11:22:46 -0500

    Make per page number of posts 5

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,9 @@ class PostsController < ApplicationController
 
   def index
     if params[:tagged]
-      @posts = Post.scoped_posts(signed_in?).tagged_with(params[:tagged]).paginate(page: params[:page])
+      @posts = Post.scoped_posts(signed_in?).tagged_with(params[:tagged]).paginate(page: params[:page], per_page: 5)
     else
-      @posts = Post.scoped_posts(signed_in?).paginate(page: params[:page])
+      @posts = Post.scoped_posts(signed_in?).paginate(page: params[:page], per_page: 5)
     end
   end
 
