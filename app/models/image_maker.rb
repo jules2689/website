140014c70b0a19a61ac11c3242e43867f8428354
Julian Nadeau <julian@jnadeau.ca>
2015-11-01 17:55:53 -0500

    Token logging

--- app/models/image_maker.rb
+++ app/models/image_maker.rb
@@ -2,6 +2,11 @@ class ImageMaker
 
   def create_image(title, path, image)
     Rails.logger.info("Creating image '#{title}'...")
+    if ENV["WEBSITE_GITHUB_KEY"].present?
+      puts "TOKEN WAS HERE"
+    else
+      puts "TOKEN MISSING"
+    end
     Julianssite::GithubClient.create_contents("jules2689/gitcdn",
                            "images/website/#{path}",
                            "Adding Image #{path}",
