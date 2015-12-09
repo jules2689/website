caaa030a07504cbae38a97738e50fcd132f7d275
Julian Nadeau <julian@jnadeau.ca>
2015-12-09 10:18:15 -0500

    https links 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,13 +54,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = Proc.new { |source, request|
-    if request && request.ssl?
-      "#{request.protocol}#{request.host_with_port}"
-    else
-      "#{request.protocol}jnadeau.ca"
-    end
-  }
+  config.action_controller.asset_host = "https://jnadeau.ca"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
