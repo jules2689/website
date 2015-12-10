9b7daa638eb4b156910f027a62b9e8219ba46dee
Julian Nadeau <julian@jnadeau.ca>
2015-12-09 21:51:20 -0500

    Clean up blog and css

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,24 +1,3 @@
 module PostsHelper
   include ActsAsTaggableOn::TagsHelper
-
-  def header_height(header_image_url)
-    image = FastImage.size(header_image_url)
-    image.present? ? image.last : 150
-  end
-
-  def top_for_header(post)
-  	top = header_height(post.header_image_url) - 120
-  	"style=\"margin-top: #{top}px !important\"".html_safe
-  end
-
-  def style_for_header_image_on_post(post)
-  	if post.header_image_url.present?
-	  	style = "style=\"background: url(#{ post.header_image_url });"
-	  	style << "-webkit-box-shadow: inset 0px -85px 78px 16px ##{ post.dominant_header_colour };"
-	  	style << "-moz-box-shadow: inset 0px -85px 78px 16px ##{ post.dominant_header_colour };"
-	  	style << "box-shadow: inset 0px -85px 78px 16px ##{ post.dominant_header_colour };"
-	  	style << "height: #{ header_height(post.header_image_url) }px !important;\""
-	  	style.html_safe
-	  end
-  end
 end
