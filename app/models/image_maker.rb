74e5fc327fe6eaaf5e25a256be260433735eee5d
Julian Nadeau <julian@jnadeau.ca>
2015-12-15 00:07:14 -0500

    Fix image maker

--- app/models/image_maker.rb
+++ app/models/image_maker.rb
@@ -1,10 +1,11 @@
 class ImageMaker
-  def create_image(title, remote_path, file_path)
-    image_path = file_path.path if file_path.respond_to?('path')
+  def create_image(title, remote_path, image_path)
+    image_path = image_path.path if image_path.respond_to?('path')
 
     Rails.logger.info("Creating image '#{title}'...")
     remote_image_path = "images/website/#{remote_path}"
-    PersonalWebsite::GithubClient.create_contents("#{Rails.application.secrets.git_cdn_repo}",
+
+    PersonalWebsite::GithubClient.create_contents(Rails.application.secrets.git_cdn_repo,
       remote_image_path,
       "Adding Image #{remote_path}",
       branch: "gh-pages",
