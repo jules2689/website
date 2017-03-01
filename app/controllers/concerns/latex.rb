54189ef4388c905c52662d721b700627f0e8349e
Julian Nadeau <julian@jnadeau.ca>
2017-02-28 21:03:39 -0500

    Clean up Latex commands

---
+++ app/controllers/concerns/latex.rb
@@ -0,0 +1,44 @@
+module Latex
+  def convert_to_dvi(uid, params)
+    # LaTeX document skeleton
+    document_top = "\\documentclass{article}
+    \\usepackage{mathtools}
+    \\usepackage{silence}
+    \\everymath{\\displaystyle}
+    \\begin{document}
+    \\scrollmode
+    \\pagestyle{empty}
+    $"
+
+    document_bottom = "$
+    \\batchmode
+    \\end{document}"
+
+    # Assemble the complete document source code (head + corp + foot)
+    complete_tex_doc = [document_top, params[:tex], document_bottom].join
+
+    # Write the tex file
+    tex_file = Tempfile.new([uid, '.tex'])
+    tex_file.write(complete_tex_doc)
+    tex_file.flush
+
+    # Convert to dvi
+    Rails.logger.info "render tex @ #{tex_file.path}"
+    output = `latex -8bit --interaction=nonstopmode -output-directory=#{TEMP_DVI} #{tex_file.path}`
+    tex_file.close # Delete the tex file
+
+    return [false, output] if $?.exitstatus != 0
+    [true, Dir["#{TEMP_DVI}/#{uid}*.dvi"].first]
+  end
+
+  def convert_to_png(uid, dvi_path)
+    png_file = Tempfile.new([uid, '.png'])
+    Rails.logger.info "render png @ #{png_file.path}"
+    output = `dvipng -T tight -D 119 -o #{png_file.path} #{dvi_path}`
+    return [false, output] if $?.exitstatus != 0
+
+    [true, png_file.path]
+  ensure
+    FileUtils.rm_rf(dvi_path)
+  end
+end
--- app/controllers/diatex_controller.rb
+++ app/controllers/diatex_controller.rb
@@ -1,5 +1,6 @@
 class DiatexController < ApplicationController
-  # http_basic_authenticate_with name: "diatex", password: Rails.application.secrets.diatex_password
+  http_basic_authenticate_with name: "diatex", password: Rails.application.secrets.diatex_password
+  include Latex
 
   def latex
     if params[:tex].blank?
@@ -7,63 +8,20 @@ class DiatexController < ApplicationController
       return
     end
 
-    # LaTeX document skeleton
-    document_top = "\\documentclass{article}
-    \\usepackage{mathtools}
-    \\usepackage{silence}
-    \\everymath{\\displaystyle}
-    \\begin{document}
-    \\scrollmode
-    \\pagestyle{empty}
-    $"
-
-    document_bottom = "$
-    \\batchmode
-    \\end{document}"
-
-    # Prepare data
     uid = Digest::MD5.hexdigest(params[:tex])
 
-    # Assemble the complete document source code (head + corp + foot)
-    complete_tex_doc = [document_top, params[:tex], document_bottom].join
-
-    # Write the tex file
-    tex_file = Tempfile.new([uid, '.tex'])
-    tex_file.write(complete_tex_doc)
-    tex_file.flush
-
-    # Convert to dvi
-    Rails.logger.info "render tex @ #{tex_file.path}"
-    output = `latex -8bit --interaction=nonstopmode -output-directory=#{TEMP_DVI} #{tex_file.path}`
-    tex_file.close # Delete the tex file
-
-    if $?.exitstatus != 0
-      render json: { error: 'latex command did not succeed', input: params[:tex], output: output }, status: 500
+    success, dvi_path = convert_to_dvi(uid, params)
+    unless success
+      render json: { error: 'latex command did not succeed', input: params[:tex], output: dvi_path }, status: 500
       return
     end
 
-    # Compute the complete dvi file path
-    dvi_path = Dir["#{TEMP_DVI}/#{uid}*.dvi"].first
-    unless File.exist?(dvi_path)
-      render json: { error: 'dvi file did not exist' }, status: 422
+    success, png_path = convert_to_png(uid, dvi_path)
+    unless success
+      render json: { error: 'dvipng command did not succeed', input: params[:tex], output: png_path }, status: 500
       return
     end
 
-    # Convert to png
-    png_file = Tempfile.new([uid, '.png'])
-    begin
-      Rails.logger.info "render png @ #{png_file.path}"
-      output = `dvipng -T tight -D 119 -o #{png_file.path} #{dvi_path}`
-      if $?.exitstatus != 0
-        render json: { error: 'dvipng command did not succeed', input: params[:tex], output: output }, status: 500
-        return
-      end
-
-      # Clean up file system
-      FileUtils.rm_rf(dvi_path)
-      send_file png_file.path, type: 'image/png', disposition: 'inline'
-    ensure
-      png_file.close
-    end
+    send_file png_path, type: 'image/png', disposition: 'inline'
   end
 end
