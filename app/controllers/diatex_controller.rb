ef71c6ed6c0942903721962757ae61f79db76d63
Julian Nadeau <julian@jnadeau.ca>
2017-02-28 21:17:42 -0500

    Handle uploading latex to github too

--- app/controllers/diatex_controller.rb
+++ app/controllers/diatex_controller.rb
@@ -9,19 +9,32 @@ class DiatexController < ApplicationController
     end
 
     uid = Digest::MD5.hexdigest(params[:tex])
+    image_maker = ImageMaker.new
+    remote_path = "images/latex/#{uid}.png"
 
+    # Check for Cache
+    if image_maker.exists?(remote_path)
+      Rails.logger.info "Already made, sending cache"
+      render json: { url: image_maker.url(remote_path) }
+      return
+    end
+
+    # Convert latex to DVI
     success, dvi_path = convert_to_dvi(uid, params)
     unless success
       render json: { error: 'latex command did not succeed', input: params[:tex], output: dvi_path }, status: 500
       return
     end
 
+    # Convert DVI to PNG
     success, png_path = convert_to_png(uid, dvi_path)
     unless success
       render json: { error: 'dvipng command did not succeed', input: params[:tex], output: png_path }, status: 500
       return
     end
 
-    send_file png_path, type: 'image/png', disposition: 'inline'
+    # Send response
+    json_hash = ImageMaker.new.create_image("#{uid}.png", remote_path, png_path.to_s)
+    render json: { url: json_hash[:url] }
   end
 end
--- app/models/image_maker.rb
+++ app/models/image_maker.rb
@@ -3,13 +3,25 @@ class ImageMaker
     image_path = image_path.path if image_path.respond_to?('path')
 
     Rails.logger.info("Creating image '#{title}'...")
-    remote_image_path = "images/website/#{remote_path}"
-
     PersonalWebsite::GithubClient.create_contents(Rails.application.app_config.git_cdn_repo,
-      remote_image_path,
+      remote_image_path(remote_path),
       "Adding Image #{remote_path}",
       branch: "gh-pages",
       file: image_path)
-    { title: title, url: "#{Rails.application.app_config.git_cdn_repo_url}#{remote_image_path}" }
+    { title: title, url: url(remote_path) }
+  end
+
+  def remote_image_path(remote_path)
+    "images/website/#{remote_path}"
+  end
+
+  def url(remote_path)
+    "#{Rails.application.app_config.git_cdn_repo_url}#{remote_image_path(remote_path)}"
+  end
+
+  def exists?(remote_path)
+    url = url(remote_path)
+    res = Net::HTTP.get_response(URI(url))
+    res.code == '200'
   end
 end
