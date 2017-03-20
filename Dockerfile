e5c9a03a4d15e38cd3384d0e0bde3169e63e65a1
Julian Nadeau <julian@jnadeau.ca>
2017-03-20 16:42:54 -0400

    Some changes for docker

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,14 @@ FROM ruby:2.3
 
 RUN apt-get update \
     && apt-get install -y --no-install-recommends \
-        postgresql-client \
+        postgresql-client nodejs \
     && rm -rf /var/lib/apt/lists/*
 
 WORKDIR /usr/src/app
 COPY Gemfile* ./
 RUN bundle install
 COPY . .
+RUN bundle exec rake assets:precompile RAILS_ENV=production SKIP_EJSON=true
 
 EXPOSE 3000
 CMD ["rails", "server", "-b", "0.0.0.0"]
--- config/application.rb
+++ config/application.rb
@@ -17,13 +17,20 @@ module PersonalWebsite
     secrets_ejson_path = Rails.root.join('config', 'secrets', "#{Rails.env}.ejson")
     if File.exist?(secrets_ejson_path)
       encrypted_json = JSON.parse(secrets_ejson_path.read)
-      public_key = encrypted_json['_public_key']
-      raise "Private key is not listed in #{private_key_path}." unless File.exist?("/opt/ejson/keys/#{public_key}")
 
-      output, status = Open3.capture2e("ejson", "decrypt", secrets_ejson_path.to_s)
-      raise "ejson: #{output}" unless status.success?
-      development_secrets = JSON.parse(output)[Rails.env].symbolize_keys
-      Rails.application.secrets.merge!(development_secrets)
+      secrets = if ENV['SKIP_EJSON']
+        encrypted_json[Rails.env].symbolize_keys
+      else
+        public_key = encrypted_json['_public_key']
+
+        private_key_path = "/opt/ejson/keys/#{public_key}"
+        raise "Private key is not listed in #{private_key_path}." unless File.exist?(private_key_path)
+
+        output, status = Open3.capture2e("ejson", "decrypt", secrets_ejson_path.to_s)
+        raise "ejson: #{output}" unless status.success?
+        JSON.parse(output)[Rails.env].symbolize_keys
+      end
+      Rails.application.secrets.merge!(secrets)
     end
   end
 end
