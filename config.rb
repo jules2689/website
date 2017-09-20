4dffd9165634d74c02af0b5810d279dc5e53e156
jules2689 <julian@jnadeau.ca>
2017-09-20 17:32:18 +0900

    Update paths

--- config.rb
+++ config.rb
@@ -1,32 +1,15 @@
-# Activate and configure extensions
-# https://middlemanapp.com/advanced/configuration/#configuring-extensions
-
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
-# Layouts
-# https://middlemanapp.com/basics/layouts/
-
-# Per-page layout changes
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
-
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
-
 proxy(
   '/kaigi.html',
-  '/presentations/kaigi.html'
+  '/presentations/rubykaigi2017.html'
 )
-
-# Helpers
-# Methods defined in the helpers block are available in templates
-# https://middlemanapp.com/basics/helper-methods/
 
 helpers do
   def iterate_folder(folder, limit: -1, sort: nil)
@@ -43,11 +26,3 @@ helpers do
     end
   end
 end
-
-# Build-specific configuration
-# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
-
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
